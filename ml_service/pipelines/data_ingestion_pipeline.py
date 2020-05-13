38704e950a08a27b6e494b4bdc4050570d2baa71
Anudeep Dhonde <anudeep.dhonde@pacteraedge.com>
2020-05-13 13:27:13 +0530

    code changes to test multiple dataset registration

--- ml_service/pipelines/data_ingestion_pipeline.py
+++ ml_service/pipelines/data_ingestion_pipeline.py
@@ -7,7 +7,7 @@ from azureml.core import Workspace, Dataset, Datastore
 
 # Data Ingestion related
 from kaggle_titanic.data_ingestion import data_preparation
-from ml_service.util.ingestion_helpers import write_files_to_local, upload_and_register_datasets
+from ml_service.util.ingestion_helpers import write_file_to_local, upload_and_register_dataset
 
 # Other utilities
 from ast import literal_eval
@@ -34,45 +34,58 @@ def main():
     else: # get the default datastore tagged to the workspace
         datastore_name = aml_workspace.get_default_datastore().name
 
-    # Read dataset name from environment variables
-    dataset_name = e.dataset_name
-
-    # Check if the dataset exists in the aml workspace
-    ds_unavailable = dataset_name not in aml_workspace.datasets
+    # Read the rerun data ingestion from environment variables
     rerun_data_ingest = literal_eval(e.fresh_data_ingest)
 
-    # If dataset is not already registered or fresh data ingest flag rerun dataset creation
-    if ds_unavailable or rerun_data_ingest:
+    # Directory to the save prepared dataset
+    ingested_data_directory = e.ingested_data_directory
 
-        if ds_unavailable:
-            print(f"Dataset {dataset_name} unavailable on datastore {datastore_name}\n"
-                  f"Dataset Creation and Registration triggered for the first time")
+    # Target path on the datastore where the data should be saved
+    target_path = e.path_on_datastore
+
+    # SubFolder name on datastore would be created as per datetime
+    sub_folder_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    folderpath_on_dstore = os.path.join(target_path, sub_folder_name, "")
+
+    # Pull Data from SQL or CSV and save it to local
+    # dict_dfs is a dictionary with key value pairs {"dataset_name":DataFrame}
+    dict_dfs = data_preparation()
+
+    # Iterate for each dataset in the dict_df
+    for key, value in dict_dfs.items():
+
+        # Assign key and values to the required variables
+        dataset_name = key
+        req_df = value
+
+        # Check if the dataset exists in the aml workspace
+        ds_unavailable = dataset_name not in aml_workspace.datasets
+
+        # If dataset is not already registered or fresh data ingest flag rerun dataset creation
+        if ds_unavailable or rerun_data_ingest:
+
+            if ds_unavailable:
+                print(f"Dataset {dataset_name} unavailable on datastore {datastore_name}\n"
+                      f"Dataset Creation and Registration triggered for the first time")
+            else:
+                print(f"Dataset {dataset_name} already exists on datastore {datastore_name}\n"
+                      f"Dataset Creation and Registration triggered due to change in data or code base")
+
+            # Write files to a local path and get full paths
+            req_full_file_path = write_file_to_local(dataset_name, req_df, ingested_data_directory)
+
+            # Upload files to the datastore
+            status = upload_and_register_dataset(req_full_file_path, dataset_name, aml_workspace,
+                                                 datastore_name, folderpath_on_dstore)
+
+            # Print based on status
+            print(f"--"*50)
+            print(f"{status.upper()}: Dataset {dataset_name} is uploaded to {datastore_name}"
+                  f" and registered on workspace {aml_workspace.name}")
+            print(f"--" * 50)
         else:
-            print(f"Dataset {dataset_name} already exists on datastore {datastore_name}\n"
-                  f"Dataset Creation and Registration triggered due to change in data or code base")
-
-        # Pull Data from SQL or CSV and save it to local
-        # dict_dfs is a dictionary with key value pairs {"dataset_name":DataFrame}
-        dict_dfs = data_preparation()
-
-        # Directory to the save prepared dataset
-        ingested_data_directory = e.ingested_data_directory
-
-        # Write files to a local path and get full paths
-        dict_comp_paths_files = write_files_to_local(dict_dfs, ingested_data_directory)
-
-        # Target path on the datastore where the data should be saved
-        target_path = e.path_on_datastore
-
-        # SubFolder name on datastore would be created as per datetime
-        sub_folder_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
-        # Upload files to the datastore
-        upload_and_register_datasets(dict_comp_paths_files, aml_workspace, datastore_name, target_path, sub_folder_name)
-
-    else:
-        print(f"Dataset {dataset_name} is already available on datastore {datastore_name}\n"
-              f"No Dataset Creation and Registration was triggered using")
+            print(f"Dataset {dataset_name} is already available on datastore {datastore_name}\n"
+                  f"No Dataset Creation and Registration was triggered using")
 
 
 if __name__ == '__main__':
--- ml_service/util/ingestion_helpers.py
+++ ml_service/util/ingestion_helpers.py
@@ -3,96 +3,88 @@ import os
 from azureml.core import Datastore, Dataset
 
 
-def write_files_to_local(dict_dfs, data_directory):
+def write_file_to_local(dataset_name, req_df, data_directory):
     """
     Function to write files to a path and return a list of file paths
-    dict_dfs        : Dictionary with key as dataset name and value as DataFrame
-    data_directory  : Location where the file should be saved
-    return          : dict of paths of all the files with key as datasetname and value as full path
+    dataset_name (str)      : Dataset name
+    req_df (DataFrame)      : DataFrame corresponding to the dataset_name
+    data_directory (str)    : Location where the file should be saved
+    return (str)            : Full path of the file written to local
     """
-
-    # Initialize a list to store complete paths
-    dict_comp_paths = {}
+    # Initialize require file path to be None
+    full_file_path = None
 
     # Get full path of directory
     directory_full_path = os.path.abspath(data_directory)
 
-    # Iterate over the dictionary:
-    for key in dict_dfs.keys():
+    # Creating a file name
+    req_file_name = dataset_name + ".csv"
 
-        # Creating a file name
-        req_file_name = key + ".csv"
+    # File path
+    req_file_path = os.path.join(directory_full_path, req_file_name)
 
-        # File path
-        req_file_path = os.path.join(directory_full_path, req_file_name)
+    try:
+        # Writing to local
+        req_df.to_csv(req_file_path)
 
-        # required dataframe is value in the dictionary
-        req_df = dict_dfs[key]
+        # if write is successful, Append the file path to the list
+        full_file_path = req_file_path
+    except Exception as ex:
+        print(ex)
 
-        try:
-            # Writing to local
-            req_df.to_csv(req_file_path)
-
-            # if write is successful, Append the file path to the list
-            dict_comp_paths[key] = req_file_path
-        except Exception as ex:
-            print(ex)
-
-    return dict_comp_paths
+    return full_file_path
 
 
-def upload_and_register_datasets(dict_comp_paths_files, aml_workspace, datastore_name, target_path, sub_folder_name):
+def upload_and_register_dataset(req_full_file_path, dataset_name, aml_workspace, datastore_name, folderpath_on_dstore):
     """
     Function to upload files to a datastore
-    dict_comp_paths_files   : Dictionary with dataset name as key and full path as value
-    aml_workspace           : AML workspace object
-    datastore_name          : Name of the datastore tagged to AML workspace
-    target_path             :Folder path on the datastore
-    sub_folder_name         : Subfolder name where the datasets should be registered
-    return                  : "success" or "failure" based on the upload status
+    req_full_file_path (str)    : Full os path of the filename
+    dataset_name (str)          : Name of the dataset with which it should be registered on aml workspace
+    aml_workspace (Workspace)   : AML workspace object
+    datastore_name (str)        : Name of the datastore tagged to AML workspace
+    folderpath_on_dstore(str)   : Subfolder name where the datasets should be registered
+    return                      : "success" or "failure" based on the upload status
     """
 
-    # Target path including subfolder
-    target_sub_folder = os.path.join(target_path, sub_folder_name, "")
+    # Status
+    status = "success"
 
-    for key, value in dict_comp_paths_files.items():
+    # Connect to the datastore object in the AML workspace
+    aml_datastore = Datastore.get(aml_workspace, datastore_name=datastore_name)
 
-        # Assigning dataset name and full path of the file
-        dataset_name = key
-        comp_file_path = value
+    try:
+        # Upload files to datastore in workspace
+        aml_datastore.upload_files(files=[req_full_file_path],  # Upload the titanic csv files in /data
+                                   target_path=folderpath_on_dstore,  # Put it in a folder path in the datastore
+                                   overwrite=True,  # Replace existing files of the same name
+                                   show_progress=True)
 
-        # Connect to the datastore object in the AML workspace
-        aml_datastore = Datastore.get(aml_workspace, datastore_name=datastore_name)
+        # Basefile name extracted from local path
+        base_filename = os.path.basename(req_full_file_path)
+
+        # Path on datastore (Joined with path on datastore and basefilename)
+        path_on_datastore = os.path.join(folderpath_on_dstore, base_filename)
+
+        # Register dataset
+        dataset = Dataset.Tabular.from_delimited_files(
+            path=(aml_datastore, path_on_datastore))
 
         try:
-            # Upload files to datastore in workspace
-            aml_datastore.upload_files(files=[comp_file_path],  # Upload the titanic csv files in /data
-                                       target_path=target_sub_folder,  # Put it in a folder path in the datastore
-                                       overwrite=True,  # Replace existing files of the same name
-                                       show_progress=True)
-
-            # Basefile name
-            base_filename = os.path.basename(comp_file_path)
-
-            # Path on datastore
-            path_on_datastore = os.path.join(target_sub_folder, base_filename)
-
-            # Register dataset
-            dataset = Dataset.Tabular.from_delimited_files(
-                path=(aml_datastore, path_on_datastore))
-
-            try:
-                dataset = dataset.register(
-                    workspace=aml_workspace,
-                    name=dataset_name,
-                    # description='titanic training data',
-                    tags={'format': 'CSV'},
-                    create_new_version=True)
-
-            except Exception as ex:
-                print(f"Error in registering datasets on datastore")
-                print(ex)
+            dataset = dataset.register(
+                workspace=aml_workspace,
+                name=dataset_name,
+                # description='titanic training data',
+                tags={'format': 'CSV'},
+                create_new_version=True)
 
         except Exception as ex:
-            print(f"Error in uploading files onto the datastore\n")
+            print(f"Error in registering dataset {dataset_name} on datastore {datastore_name}")
             print(ex)
+            status = "failure"
+
+    except Exception as ex:
+        print(f"Error in uploading file {req_full_file_path} onto the datastore {datastore_name}\n")
+        print(ex)
+        status = "failure"
+
+    return status
