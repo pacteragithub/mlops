ff0440bf52b9dcebbbfcdbf18ce88527e400822a
ShaikBasha1247 <shaiknagoorbasha.nb@gmail.com>
2020-06-08 17:28:43 +0530

    Update cleanse.py

--- kaggle_titanic/data_ingestion/cleanse.py
+++ kaggle_titanic/data_ingestion/cleanse.py
@@ -56,4 +56,3 @@ if not (step_output_path is None):
     os.makedirs(step_output_path, exist_ok=True)
     full_output_path = os.path.join(step_output_path, "cleaned.csv")
     write_df = cleaned_df.to_csv(full_output_path)
-#end of the file
