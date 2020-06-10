628bc7ea094d1648a06ff4b8f37d768d0fa64204
Anudeep Dhonde <anudeep.dhonde@pacteraedge.com>
2020-06-10 11:09:36 +0530

    added feature engineering code for Title and Status

--- kaggle_titanic/feat_eng/feat_create.py
+++ kaggle_titanic/feat_eng/feat_create.py
@@ -1,3 +1,25 @@
+from sklearn.preprocessing import LabelEncoder
+
+
+def get_status(df, Title_Dictionary):
+    # a map of more aggregated title
+    # we map each title
+    df['Status'] = df["Title"].map(lambda x: Title_Dictionary[x])
+    return df
+
+
+def add_title_features(df):
+    Title_Dictionary = {"Capt": "Officer", "Col": "Officer", "Major": "Officer", "Jonkheer": "Royalty",
+                        "Don": "Royalty", "Sir": "Royalty", "Dr": "Officer", "Rev": "Officer",
+                        "the Countess": "Royalty", "Mme": "Mrs", "Mlle": "Miss", "Ms": "Mrs","Mr": "Mr",
+                        "Mrs": "Mrs", "Miss": "Miss", "Master": "Master", "Lady": "Royalty"}
+
+    df['Title'] = df['Name'].map(lambda name: name.split(',')[1].split('.')[0].strip())
+    print(df["Title"].isnull().sum())
+
+    new_df = get_status(df, Title_Dictionary)
+
+    return new_df
 
 
 def create_new_features(clean_df):
@@ -10,12 +32,29 @@ def create_new_features(clean_df):
     genders = {"male": 0, "female": 1}
     clean_df['Sex'] = clean_df['Sex'].map(genders)
 
-    # Keep only these columns for modeling
-    X_cols = ['Passenger_Class', 'Sex', 'SibSp', 'Parch', 'Fare']
-    Y_col = ["Survived"]
+    # use engineered features
+    use_eng_features = False
 
-    # Subsetting and keeping only those columns
-    req_df = clean_df[X_cols+Y_col].copy()
+    if use_eng_features:
+        # Engineered features
+        eng_df = add_title_features(clean_df)
+        le = LabelEncoder()
+        eng_df['Title'] = le.fit_transform(eng_df['Title'])
+        eng_df['Status'] = le.fit_transform(eng_df['Status'])
+
+        # Keep only these columns for modeling
+        X_cols = ['Passenger_Class', 'Sex', 'SibSp', 'Parch', 'Fare', 'Title', 'Status']
+        Y_col = ["Survived"]
+
+        req_df = eng_df[X_cols + Y_col].copy()
+
+    else:
+        # Keep only these columns for modeling
+        X_cols = ['Passenger_Class', 'Sex', 'SibSp', 'Parch', 'Fare']
+        Y_col = ["Survived"]
+
+        eng_df = clean_df.copy()
+        req_df = eng_df[X_cols + Y_col].copy()
 
     return req_df
 
